3604c553316e37e792714161a08d54050beef55b
mr.zhang <mr.zy883@gmail.com>
2020-06-09 10:59:09 +0800

    Bee Framework Config基础配置文件

---
+++ src/app/config/validator/validator-config.ts
@@ -0,0 +1,12 @@
+/**
+ * BeeFramework 定义验证器 Key值
+ */
+export const validatorKeys = {
+  //BeeFramework 自定义
+  beeRequired: 'bee-required',
+  beeCheck: 'bee-check',
+
+  //angular
+  ngRequired: 'required'
+}
+
